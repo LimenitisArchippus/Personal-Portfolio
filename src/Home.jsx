3914058b852a50b96f78a0c6c409952e758e7145
Jonathan Pope <52277065+LimenitisArchippus@users.noreply.github.com>
2025-05-16 23:37:39 -0600

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { contRender } from './Main.jsx';
+
+function Button({ onButtonClick }) {
+  return (
+    <>
+      <button
+        className="bg-green-500 text-white py-3 px-6 rounded-full hover:bg-green-700 transition duration-300 ease-in-out text-lg mt-3"
+        onClick={onButtonClick}
+      >
+        Shop Now!
+      </button>
+    </>
+  );
+}
+
+function Home() {
+  function renderProducts() {
+    contRender('Products');
+  }
+
+  return (
+    <>
+      <section id="home" className="text-center">
+        <div className="m-6 p-6 p-600 bg-aliceblue rounded-lg shadow-md">
+          <h2 className="text-3xl font-semibold text-gray-800 ">
+            Hello, Welcome to my Personal Portfolio
+          </h2>
+          <p className="text-lg text-gray-700">
+            This Website Serves as my Portfolio for Business and Professional Use
+          </p>
+        </div>
+        <div className="m-6 p-6 bg-aliceblue rounded-lg shadow-md">
+          <p>Hello, this portfolio is all about me and my work within software development and Programming, from projects to my school education.
+          </p>
+          <p>Contained within this website is an about page, with details about me, my life, my work, and my passion for programming. Also contained is a contact page, an examples page (with links to a few select projects), and an experience page which contains information regarding my education and programming knowledge.</p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
